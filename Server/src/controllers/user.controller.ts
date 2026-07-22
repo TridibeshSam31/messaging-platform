1eb0a1af02d3dcb99ea439db337c6fa2a9138989
TridibeshSam31 <tridisam651@gmail.com>
2026-07-23 03:44:23 +0530

    userController done

--- Server/src/controllers/user.controller.ts
+++ Server/src/controllers/user.controller.ts
@@ -34,7 +34,7 @@ Outputs: 200 OK with user list.
 import { Request, Response, NextFunction } from "express";
 import { UserService } from "../services/user.service.js";
 import{updateProfileSchema} from "../types/index.js"
-
+import z from "zod"
 export class UserController{
     static async getMe(req:Request , res:Response , next:NextFunction){
 
@@ -51,7 +51,47 @@ export class UserController{
     }
 
     static async PatchMe(req:Request , res:Response , next:NextFunction){
+
+        try{
+
+            const userProfileParse = updateProfileSchema.safeParse(req.body)
+            if(!userProfileParse.success){
+                return res.status(400).json({error: userProfileParse.error})
+            }
+            const updated = await UserService.updateProfile(req.userId!, userProfileParse.data as { name?: string; username?: string; });
+            return res.status(200).json({updated})
+
+        }catch(error){
+            next(error)
+        }
+
+        
         
     }
+
+
+    static async patchAvatar(req: Request, res: Response, next: NextFunction) {
+    try {
+        //@ts-ignore
+      if (!req.file) {
+        return res.status(400).json({ error: "No file uploaded" });
+      }
+      //@ts-ignore
+      const avatarUrl = `/uploads/${req.file.filename}`;
+      const updated = await UserService.updateAvatar(req.userId!, avatarUrl);
+      return res.status(200).json(updated);
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async getSearch(req: Request, res: Response, next: NextFunction) {
+    try {
+      const query = z.string().parse(req.query.q);
+      const users = await UserService.searchUsers(query, req.userId!);
+      return res.status(200).json(users);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
