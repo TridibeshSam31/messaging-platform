25a1854e982ddb3e731f0b16cf4b43c29ed83eec
TridibeshSam31 <tridisam651@gmail.com>
2026-07-25 01:51:04 +0530

    adding some summary in index.ts about what I am going to write

---
+++ Server/src/socket/index.ts
@@ -0,0 +1,7 @@
+//will not be using socket.io library 
+//I will write the alternate code at the end so that to understand well and use the same sort of codes everywhere
+//creating raw websocker servers with socket.io library 
+
+//since there are a lot of features like 
+//online_presence , offiline_presence , send_Message etc yeh saari cheje hogi
+//this is somehow difficult because there will be aleast 4 sets like socket , user , rooms , online_presence 
