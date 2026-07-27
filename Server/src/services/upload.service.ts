5ad4f19208135e04b9bcc0388a3e4de32c20cc78
TridibeshSam31 <tridisam651@gmail.com>
2026-07-27 23:04:29 +0530

    done

--- Server/src/services/upload.service.ts
+++ Server/src/services/upload.service.ts
@@ -1,5 +1,7 @@
-import cloudinary from "cloudinary"
-import streamifier from "streamifier"
+import { v2 as cloudinary } from "cloudinary"
+const streamifier = require("streamifier") as {
+  createReadStream(buffer: Buffer): NodeJS.ReadableStream;
+};
 
 /*
 
@@ -8,6 +10,71 @@ while Cloudinary's upload_stream() API accepts a Readable Stream, not a Buffer.
 streamifier.createReadStream(file.buffer) converts the in-memory Buffer into a stream, allowing the binary data to be piped directly to Cloudinary without first saving it to disk. 
 It is not used to reduce memory usage here—the file is already in memory—but simply to make the Buffer compatible with Cloudinary's streaming API, enabling efficient uploads while avoiding temporary files.
 
+Client
+   │
+POST /upload
+   │
+upload middleware (multer)
+   │
+Upload Controller
+   │
+Upload Service
+   │
+Cloudinary
+   │
+Return attachment metadata
 
+
+Controller
+    │
+uploadAttachment(file)
+    │
+Buffer
+    │
+AWS SDK (PutObjectCommand)
+    │
+S3 Bucket
+    │
+Object URL + Key
 
 */
+
+
+
+
+interface UploadResult {
+  url: string;
+  publicId: string;
+  mimeType: string;
+  size: number;
+  fileName: string;
+}
+
+export const uploadFile = (
+  file: Express.Multer.File
+): Promise<UploadResult> => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: "chat-app",
+        resource_type: "auto",
+      },
+      (error:any, result:any) => {
+        if (error || !result) {
+          return reject(error);
+        }
+
+        resolve({
+          url: result.secure_url,
+          publicId: result.public_id,
+          mimeType: file.mimetype,
+          size: file.size,
+          fileName: file.originalname,
+        });
+      }
+    );
+
+    streamifier.createReadStream(file.buffer).pipe(uploadStream);
+  });
+};
+
