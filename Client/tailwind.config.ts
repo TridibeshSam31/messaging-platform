f3472a87f7a5594bc221df2814d05e7e9f38ebe7
TridibeshSam31 <tridisam651@gmail.com>
2026-08-09 01:25:10 +0530

    installing dependencies , swtting up shadcn and updating tailwind etc

--- Client/tailwind.config.ts
+++ Client/tailwind.config.ts
@@ -1,5 +1,6 @@
 // tailwind.config.ts
 import type { Config } from "tailwindcss"
+import animatePlugin from "tailwindcss-animate"
 
 const config: Config = {
   darkMode: ["class"],
@@ -50,7 +51,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animatePlugin],
 }
 
 export default config
